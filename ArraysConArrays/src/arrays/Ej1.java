e90575ee6bd9f6d3206ce0d70f1047dd5b186612
Siobhantt <luisa.cardozo@iesnervion.es>
2022-12-14 00:09:50 +0100

    Primer ejercicio completo; segundo ejercicio por resolver

---
+++ ArraysConArrays/src/arrays/Ej1.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ej1 {
+/*Crea un programa que pida al usuario dos valores N y M y luego cree un array de
+tamaño N que contenga M en todas sus posiciones. Luego muestra el array por
+pantalla.*/
+	public static void main(String[] args) {
+		
+		int valorN, valorM;
+		
+		Scanner lee = new Scanner(System.in);
+		
+		System.out.println("Por favor introduzca el tamaño de la tabla: ");
+		valorN=lee.nextInt();
+		System.out.println("Por favor introduzca el valor con el que quiere llenar cada posicion: ");
+		valorM=lee.nextInt();
+		
+		int tabla[] = new int [valorN];
+		
+		Arrays.fill(tabla, valorM);
+		
+		System.out.println(Arrays.toString(tabla));
+	//cerramos el scanner
+	lee.close();
+	}
+
+
+}
