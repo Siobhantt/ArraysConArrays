7b7e1671ed33bd40d844467f437f2ee6df036b79
lcardozo <lcardozo@106-25.info2>
2022-12-21 10:00:57 +0100

    4 listo

---
+++ ArraysConArrays/src/arrays/Ej4.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Ej4 {
+/*Crea un programa que cree un array de tamaño 30 y lo rellene con valores aleatorios entre 0 y 9 (utiliza Math.random()*10). 
+ * Luego ordena los valores del array y los mostrará por pantalla.*/
+	public static void main(String[] args) {
+	
+		int tabla[] = new int [30];
+		
+		for (int i =0; i <tabla.length; i++) {
+			tabla[i] = (int) (Math.random()*10);
+		}
+		System.out.print("Tabla desordenada");
+		System.out.println(Arrays.toString(tabla));
+		Arrays.sort(tabla);
+		System.out.println();
+		System.out.print("Tabla ordenada");
+		System.out.println(Arrays.toString(tabla));
+
+	}
+
+}
