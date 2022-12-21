53d7bb3ee537955ecda51d6b02e3666cc236f127
lcardozo <lcardozo@106-25.info2>
2022-12-21 10:03:32 +0100

    Todo ordenado (:

--- ArraysConArrays/src/arrays/Ej1.java
+++ ArraysConArrays/src/arrays/Ej1.java
@@ -4,28 +4,29 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ej1 {
-/*Crea un programa que pida al usuario dos valores N y M y luego cree un array de
-tamaño N que contenga M en todas sus posiciones. Luego muestra el array por
-pantalla.*/
+	/*
+	 * Crea un programa que pida al usuario dos valores N y M y luego cree un array
+	 * de tamaño N que contenga M en todas sus posiciones. Luego muestra el array
+	 * por pantalla.
+	 */
 	public static void main(String[] args) {
-		
-		int valorN, valorM;
-		
-		Scanner lee = new Scanner(System.in);
-		
-		System.out.println("Por favor introduzca el tamaño de la tabla: ");
-		valorN=lee.nextInt();
-		System.out.println("Por favor introduzca el valor con el que quiere llenar cada posicion: ");
-		valorM=lee.nextInt();
-		
-		int tabla[] = new int [valorN];
-		
-		Arrays.fill(tabla, valorM);
-		
-		System.out.println(Arrays.toString(tabla));
-	//cerramos el scanner
-	lee.close();
-	}
 
+		int valorN, valorM;
+
+		Scanner lee = new Scanner(System.in);
+
+		System.out.println("Por favor introduzca el tamaño de la tabla: ");
+		valorN = lee.nextInt();
+		System.out.println("Por favor introduzca el valor con el que quiere llenar cada posicion: ");
+		valorM = lee.nextInt();
+
+		int tabla[] = new int[valorN];
+
+		Arrays.fill(tabla, valorM);
+
+		System.out.println(Arrays.toString(tabla));
+		// cerramos el scanner
+		lee.close();
+	}
 
 }
--- ArraysConArrays/src/arrays/Ej4.java
+++ ArraysConArrays/src/arrays/Ej4.java
@@ -3,14 +3,17 @@ package arrays;
 import java.util.Arrays;
 
 public class Ej4 {
-/*Crea un programa que cree un array de tamaño 30 y lo rellene con valores aleatorios entre 0 y 9 (utiliza Math.random()*10). 
- * Luego ordena los valores del array y los mostrará por pantalla.*/
+	/*
+	 * Crea un programa que cree un array de tamaño 30 y lo rellene con valores
+	 * aleatorios entre 0 y 9 (utiliza Math.random()*10). Luego ordena los valores
+	 * del array y los mostrará por pantalla.
+	 */
 	public static void main(String[] args) {
-	
-		int tabla[] = new int [30];
-		
-		for (int i =0; i <tabla.length; i++) {
-			tabla[i] = (int) (Math.random()*10);
+
+		int tabla[] = new int[30];
+
+		for (int i = 0; i < tabla.length; i++) {
+			tabla[i] = (int) (Math.random() * 10);
 		}
 		System.out.print("Tabla desordenada");
 		System.out.println(Arrays.toString(tabla));
